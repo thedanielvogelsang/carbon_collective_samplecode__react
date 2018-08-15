5bc1d96c59119e79032c71b5804fd9b74d256b55
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-08-15 14:55:27 -0600

    properly adds redux extension

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
 const initialState = {}
 const middleware = [thunk]
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+const store = createStore(rootReducer, initialState, compose(
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 export default store;
